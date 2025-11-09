9ae06a52afb72ddec63c6152e001b60525edbdb9
Celestino Agudo <celestinoagudo07@gmail.com>
2025-11-10 02:02:10 +0800

    server streaming blocking tesT

---
+++ grpc-playground/src/test/java/grind/twofourseven/unary/client/ServerStreamingTest.java
@@ -0,0 +1,26 @@
+package grind.twofourseven.unary.client;
+
+import grind.twofourseven.model.unary.WithdrawRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ServerStreamingTest extends AbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServerStreamingTest.class);
+
+    @Test
+    void blockingClientWithdrawTest() {
+        var request = WithdrawRequest.newBuilder().setAmount(20).setAccountNumber(1)
+                .build();
+        var iterator = blockingStub.withdraw(request);
+        var count = 0;
+        while (iterator.hasNext()) {
+            LOGGER.info("Received Money: {}", iterator.next());
+            ++count;
+        }
+        Assertions.assertEquals(2, count);
+    }
+
+}
