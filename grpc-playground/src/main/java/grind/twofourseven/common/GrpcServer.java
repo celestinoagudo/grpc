cb03a7bef6d7d358173ed1004c6df66ca8f6c84e
Celestino Agudo <celestinoagudo07@gmail.com>
2025-12-14 18:03:00 +0800

    semicolon I forgot

--- grpc-playground/src/main/java/grind/twofourseven/common/GrpcServer.java
+++ grpc-playground/src/main/java/grind/twofourseven/common/GrpcServer.java
@@ -22,7 +22,7 @@ public class GrpcServer {
     }
 
     public static GrpcServer create(final int port, final BindableService... services) {
-        var builder = ServerBuilder.forPort(port).keepAliveTime(10, TimeUnit.SECONDS)
+        var builder = ServerBuilder.forPort(port).keepAliveTime(10, TimeUnit.SECONDS);
 //                .keepAliveTimeout(1, TimeUnit.SECONDS)
 //                .maxConnectionIdle(25, TimeUnit.SECONDS); // if there's no RPC, send go away.
         Arrays.asList(services).forEach(builder::addService);
