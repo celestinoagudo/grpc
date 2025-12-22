d6d1dd911eae107f60afd0e868648a47a487ac64
Celestino Agudo <celestinoagudo07@gmail.com>
2025-12-22 23:15:57 +0800

    grpc with springboot initial files

---
+++ grpc-springboot/src/test/java/com/frind/two/four/seven/grpc/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frind.two.four.seven.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
